6224db56e8f5cc2abaa2500b5fca3dfa4fab3613
noshinrasa <noshin.raisa@stonybrook.edu>
2023-04-23 16:57:39 +0000

    i add this html file

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('home.html')
+# Two different medical codexes data
+codex1 = {
+    "A": "Diabetes mellitus",
+    "B": "Hypertension",
+    "C": "Coronary artery disease"
+}
+codex2 = {
+    "1": "Infectious and parasitic diseases",
+    "2": "Neoplasms",
+    "3": "Diseases of the blood and blood-forming organs"
+}
 
-# app.run(host='0.0.0.0', port=5000, debug=True)
+@app.route('/')
+def index():
+    return render_template('index.html', codex1=codex1, codex2=codex2)
+
+@app.route('/api/codex1')
+def get_codex1():
+    return jsonify(codex1)
+
+@app.route('/api/codex2')
+def get_codex2():
+    return jsonify(codex2)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
